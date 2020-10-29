d7a6caef39a8af1747acb6a12df49e9faa7b7f50
Igor Mandrichenko <igorvm@gmail.com>
2020-10-29 11:00:24 -0500

    .

--- ui/metacat_ui.py
+++ ui/metacat_ui.py
@@ -18,53 +18,57 @@ Usage:
         query
 """
 
-opts, args = getopt.getopt(sys.argv[1:], "c:s:")
-opts = dict(opts)
+def main():
 
-server_addr = opts.get("-s", os.environ.get("METACAT_SERVER"))
-if server_addr:
-    host, port = server_addr.split(":", 1)
-    server_addr = (host, int(port))
+    opts, args = getopt.getopt(sys.argv[1:], "c:s:")
+    opts = dict(opts)
 
-cfg_file = opts.get("-c", os.environ.get("METACAT_CONFIG"))    
-if not cfg_file or not args or args[0] == "help":
-    print (Usage)
-    sys.exit(2)
+    server_addr = opts.get("-s", os.environ.get("METACAT_SERVER"))
+    if server_addr:
+        host, port = server_addr.split(":", 1)
+        server_addr = (host, int(port))
+
+    cfg_file = opts.get("-c", os.environ.get("METACAT_CONFIG"))    
+    if not cfg_file or not args or args[0] == "help":
+        print (Usage)
+        sys.exit(2)
     
-config = MetaCatConfig(cfg_file)
-cmd, args = args[0], args[1:]
+    config = MetaCatConfig(cfg_file)
+    cmd, args = args[0], args[1:]
 
-if cmd == "admin":
-    # does not require server configuration
-    from metacat_admin import do_admin
-    do_admin(config, args)
-    sys.exit(0)
+    if cmd == "admin":
+        # does not require server configuration
+        from metacat_admin import do_admin
+        do_admin(config, args)
+        sys.exit(0)
 
 
 
-if server_addr:
-    server_url = "http://%s:%s" % server_addr
-else:
-    server_url = config["Server"]["URL"]
+    if server_addr:
+        server_url = "http://%s:%s" % server_addr
+    else:
+        server_url = config["Server"]["URL"]
 
-if cmd == "query":
-    from metacat_query import do_query
-    do_query(config, server_url, args)
-elif cmd == "auth":
-    from metacat_auth import do_auth
-    do_auth(config, server_url, args)
-elif cmd == "dataset":
-    from metacat_dataset import do_dataset
-    do_dataset(config, server_url, args)
-elif cmd == "file":
-    from metacat_file import do_file
-    do_file(config, server_url, args)
-elif cmd == "namespace":
-    from metacat_namespace import do_namespace
-    do_namespace(config, server_url, args)
+    if cmd == "query":
+        from metacat_query import do_query
+        do_query(config, server_url, args)
+    elif cmd == "auth":
+        from metacat_auth import do_auth
+        do_auth(config, server_url, args)
+    elif cmd == "dataset":
+        from metacat_dataset import do_dataset
+        do_dataset(config, server_url, args)
+    elif cmd == "file":
+        from metacat_file import do_file
+        do_file(config, server_url, args)
+    elif cmd == "namespace":
+        from metacat_namespace import do_namespace
+        do_namespace(config, server_url, args)
     
-else:
-    print(Usage)
-    sys.exit(2)
+    else:
+        print(Usage)
+        sys.exit(2)
     
+if __name__ == "__main__":
+    main()
     
