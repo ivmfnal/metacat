2ba1cc3668ee3eba2b42eb33c25881d881f31e54
Igor Mandrichenko <igorvm@gmail.com>
2020-10-31 06:54:43 -0500

    .

--- conversion/preload_detector_config.sh
+++ conversion/preload_detector_config.sh
@@ -12,7 +12,7 @@ create temp view active_files as
 
 
 create temp view string_attrs as
-    select f.file_id as file_id, pc.param_category || '.' || pt.param_type as name, pv.param_value as value
+    select f.file_id as file_id, pc.param_category as category, pt.param_type as name, pv.param_value as value
                                 from active_files f
                                 inner join data_files_param_values dfv on f.file_id = dfv.file_id
                                 inner join param_values pv on pv.param_value_id = dfv.param_value_id
@@ -20,16 +20,19 @@ create temp view string_attrs as
                                 inner join data_types dt on dt.data_type_id = pt.data_type_id
                                 inner join param_categories pc on pc.param_category_id = pt.param_category_id
                                 where dt.data_type = 'string'
+                                
+                                        and category='DUNE_data' and name = 'detector_config'
+                                    
 ;
 
 copy ( 
     select file_id, array_to_json(regexp_split_to_array(value, ':'))::jsonb
         from string_attrs
-        where name = 'DUNE_data.detector_config' and substr(value, 1, 1) != '{'
+        where substr(value, 1, 1) != '{'
     union
     select file_id, value::jsonb
         from string_attrs
-        where name = 'DUNE_data.detector_config' and substr(value, 1, 1) = '{'
+        where substr(value, 1, 1) = '{'
     ) 
 to stdout;
 _EOF_
