c486b03caa3624ac45389df7aea5534f0a7739d5
Igor Mandrichenko <igorvm@gmail.com>
2020-12-23 13:20:38 -0600

    .

--- conversion/restore_authenticators.sh
+++ conversion/restore_authenticators.sh
@@ -8,9 +8,11 @@ create temp table temp_auth( username text, auth_info jsonb );
 \copy temp_auth(username, auth_info) from 'data/auth_info.csv'
 
 update users u
-    set auth_info=u.auth_info || (
-        select a.auth_info from temp_auth a where a.username=u.username
-    );
-
+    set auth_info=coalesce(u.auth_info, '{}'::json) ||
+        (
+            select a.auth_info from temp_auth a where a.username=u.username
+        )
+        where exists ( select * from temp_auth a where a.username = u.username)
+;
 
 _EOF_
