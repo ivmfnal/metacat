2362d1b41105b21c5d6d0f830787e33130064876
Igor Mandrichenko <ivm@fnal.gov>
2022-03-22 08:40:29 -0500

    .

--- auth_server/docker/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker=docker
-
-if [ "$1" == "podman" ]; then
-	docker=podman
-fi
-
-$docker build -t metacat_auth_server .
--- auth_server/docker/metacat/env.py
+++
@@ -1,8 +0,0 @@
-from webpie import WPApp
-import sys
-
-def env(request, relpath):
-	out = [f"Python version: {sys.version}\n"]
-	return out + [f"{k}:\t{v}\n" for k, v in request.environ.items()], "text/plain"
-
-application = WPApp(env)
