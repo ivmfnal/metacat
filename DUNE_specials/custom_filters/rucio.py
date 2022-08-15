58b406e905c38062d1c13194a7aae4793f524dff
Igor Mandrichenko <ivm@fnal.gov>
2022-08-15 11:10:40 -0400

    code clean-up

--- DUNE_specials/custom_filters/rucio.py
+++ DUNE_specials/custom_filters/rucio.py
@@ -1,6 +1,5 @@
 import os
 from metacat.filters import MetaCatFilter
-from metacat.util import Tracer
 
 class RucioReplicas(MetaCatFilter):
     
@@ -8,31 +7,19 @@ class RucioReplicas(MetaCatFilter):
         self.RucioConfig = config.get("rucio_config")
     
     def filter(self, inputs, *params, **ignore):
-        T = Tracer()
         from rucio.client.replicaclient import ReplicaClient
         if self.RucioConfig is not None:
             os.environ["RUCIO_CONFIG"] = self.RucioConfig
         client = ReplicaClient()
-        with T["cnunk_loop"]:
-            for chunk in inputs[0].chunked():
-                chunk_files = { f.did(): f for f in chunk }
-                dids = [{"scope":f.Namespace, "name":f.Name} for f in chunk]
-                # no RSEs selected - include all files
-                for f in chunk_files.values():
-                    f.Metadata["rucio.rses"] = []
-                with T["list_replicas()"]:
-                    replicas = list(client.list_replicas(dids, all_states=False, ignore_availability=False,
-                        resolve_archives=False))
-                with T["update_meta"]:
-                    for r in replicas:
-                        with T["update_meta_single"]:
-                                did = "%(scope)s:%(name)s" % r
-                                f = chunk_files[did]
-                                f.Metadata["rucio.rses"] = list(r["rses"])
-                with T["yielding"]:
-                    for f in chunk_files.values():
-                        yield f
-        #print("trace: ", T.formatStats())
-                
-
-
+        for chunk in inputs[0].chunked():
+            chunk_files = {f.did(): f for f in chunk}
+            dids = [{"scope":f.Namespace, "name":f.Name} for f in chunk]
+            for f in chunk_files.values():
+                f.Metadata["rucio.rses"] = []
+            replicas = client.list_replicas(dids, all_states=False, ignore_availability=False,
+                resolve_archives=False)
+            for r in replicas:
+                did = "%(scope)s:%(name)s" % r
+                f = chunk_files[did]
+                f.Metadata["rucio.rses"] = list(r["rses"].keys())
+            yield from chunk_files.values()
