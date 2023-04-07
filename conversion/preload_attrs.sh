f27f5260e2ea796060aea57d1b4073782b423c8b
Igor Mandrichenko <ivm@fnal.gov>
2023-04-07 13:41:37 -0400

    .

--- conversion/preload_attrs.sh
+++ conversion/preload_attrs.sh
@@ -65,47 +65,6 @@ copy (
 ) to stdout;
 
 
--- int attrs
-copy (
-	select f.file_id, pc.param_category || '.' || pt.param_type, param_value
-                from active_files f
-                inner join num_data_files_param_values dfv on f.file_id = dfv.file_id
-                inner join param_types pt on pt.param_type_id = dfv.param_type_id
-                inner join data_types dt on dt.data_type_id = pt.data_type_id
-                inner join param_categories pc on pc.param_category_id = pt.param_category_id
-                where dt.data_type_id = 5
-                    and param_value is not null
-
-) to stdout;
-
-
--- float attrs
-copy (
-	select f.file_id, pc.param_category || '.' || pt.param_type, param_value
-                from active_files f
-                inner join num_data_files_param_values dfv on f.file_id = dfv.file_id
-                inner join param_types pt on pt.param_type_id = dfv.param_type_id
-                inner join data_types dt on dt.data_type_id = pt.data_type_id
-                inner join param_categories pc on pc.param_category_id = pt.param_category_id
-                where dt.data_type_id = 6
-                    and param_value is not null
-) to stdout;
-
--- string attrs
-copy (
-   select f.file_id, pc.param_category || '.' || pt.param_type, to_json(pv.param_value)
-                from active_files f
-                inner join data_files_param_values dfv on f.file_id = dfv.file_id
-                inner join param_values pv on pv.param_value_id = dfv.param_value_id
-                inner join param_types pt on pt.param_type_id = dfv.param_type_id
-                inner join data_types dt on dt.data_type_id = pt.data_type_id
-                inner join param_categories pc on pc.param_category_id = pt.param_category_id
-                where dt.data_type = 'string'
-                    and pv.param_value is not null
-) to stdout;
-
-
-
 _EOF_
 
 preload_json_meta ./data/attrs.csv
