1df5d70335652525cdce51b9e59ecfd75f33c8a7
Igor Mandrichenko <ivm@fnal.gov>
2023-04-08 18:12:45 -0400

    .

--- conversion/preload_dimensions_text.sh
+++ conversion/preload_dimensions_text.sh
@@ -13,15 +13,14 @@ create temp view active_files as
 -- string attrs
 copy (
    select f.file_id, pc.param_category || '.' || pt.param_type, to_json(pv.param_value)
-                from active_files f
+        from active_files f
                 inner join data_files_param_values dfv on f.file_id = dfv.file_id
                 inner join param_values pv on pv.param_value_id = dfv.param_value_id
                 inner join param_types pt on pt.param_type_id = dfv.param_type_id
                 inner join data_types dt on dt.data_type_id = pt.data_type_id
                 inner join param_categories pc on pc.param_category_id = pt.param_category_id
-        where dt.data_type = 'string'
-            and pv.param_value is not null
-        odrer by f.file_id
+        where dt.data_type = 'string' and pv.param_value is not null
+        order by f.file_id
 ) to stdout;
 
 
