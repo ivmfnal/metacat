dbcf12663678705768ba4f5cf530fcb367f29e40
Igor Mandrichenko <ivm@fnal.gov>
2023-04-09 11:45:20 -0400

    .

---
+++ conversion/create_namespaces.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source ./config.sh
+
+$OUT_DB_PSQL << _EOF_
+
+drop table if exists namespaces cascade;
+
+create table namespaces
+(
+    name                text    primary key,
+    check( name != ''),
+
+    description         text,
+
+    owner_user          text,
+    owner_role          text,
+    check ( (owner_user is null ) != (owner_role is null) ),
+
+    creator        text,
+    created_timestamp   timestamp with time zone        default now()
+);
+
+insert into namespaces(name, owner_role, creator) values('${default_namespace}', 'admin_role', 'admin')
+on conflict(name) do nothing;
+
+insert into namespaces(name, owner_role, creator)
+(
+    select distinct namespace, 'admin_role', 'admin' from files
+)
+on conflict(name) do nothing;
+
+_EOF_
+
+
+
--- conversion/create_other_tables.sh
+++ conversion/create_other_tables.sh
@@ -3,51 +3,8 @@
 source ./config.sh
 
 $OUT_DB_PSQL << _EOF_
-\set on_error_stop on
 
-drop table if exists 
-    queries
-    ,files_datasets
-    ,datasets
-    ,namespaces
-    cascade
-;
-
---drop table if exists authenticators;
---create table authenticators
---(
---    username    text    references users(username) on delete cascade,
---    type        text
---        constraint authenticator_types check ( 
---            type in ('x509','password','ssh')
---            ),
---    secrets      text[],
---    primary key(username, type)
---);
-
-create table namespaces
-(
-    name                text    primary key,
-    check( name != ''),
-
-    description         text,
-
-    owner_user          text,
-    owner_role          text,
-    check ( (owner_user is null ) != (owner_role is null) ),
-
-    creator        text,
-    created_timestamp   timestamp with time zone        default now()
-);
-
-insert into namespaces(name, owner_role, creator) values('${default_namespace}', 'admin_role', 'admin')
-on conflict(name) do nothing;
-
-insert into namespaces(name, owner_role, creator)
-(
-    select distinct namespace, 'admin_role', 'admin' from files
-)
-on conflict(name) do nothing;
+drop table if exists queries cascade;
 
 create table queries
 (
