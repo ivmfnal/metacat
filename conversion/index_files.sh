8c2a9c9a420a36adc4cc05722b939210c8cb5913
Igor Mandrichenko <ivm@fnal.gov>
2023-04-09 21:46:57 -0400

    .

--- conversion/index_files.sh
+++ conversion/index_files.sh
@@ -4,14 +4,16 @@ source ./config.sh
 
 $OUT_DB_PSQL << _EOF_
 
+drop index if exists files_names, files_name, files_size,
+    files_creator, files_created_timestamp, files_size;
+
 \echo ... building files namespace:name index ...
 create unique index files_names on files(namespace, name);
 create index files_name on files(name);
 create index files_size on files(size);
 
-\echo ... indexing file attributes ...
+
 create index files_creator on files(creator);
 create index files_created_timestamp on files(created_timestamp);
-create index files_size on files(size);
 
 _EOF_
