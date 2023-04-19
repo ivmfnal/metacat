c42f878a69d557f4f27d597a9cfaa5803b62c138
Igor Mandrichenko <ivm@fnal.gov>
2023-04-19 13:09:05 -0500

    Fixed conversion/finalize.sh

--- conversion/finalize.sh
+++ conversion/finalize.sh
@@ -2,4 +2,13 @@
 
 source ./config.sh
 
-finalize
+$OUT_DB_PSQL << _EOF_
+        create user ${WEB_USER};
+        grant all on schema ${DST_SCHEMA} to ${WEB_USER};
+        grant all on all tables in schema ${DST_SCHEMA} to ${WEB_USER};
+        \echo
+        \echo Finalized
+        \echo
+_EOF_
+
+
