a1ea2509c90ce0c9d5d5e9c92ef7fe18437e6bfd
Igor Mandrichenko <ivm@fnal.gov>
2023-05-25 15:04:55 -0400

    .

--- DUNE_specials/custom_filters/rucio_rses.py
+++ DUNE_specials/custom_filters/rucio_rses.py
@@ -24,14 +24,13 @@ class RucioReplicas(MetaCatFilter):
             os.environ["RUCIO_CONFIG"] = self.RucioConfig
         client = ReplicaClient()
 
-        for chunk in inputs[0].chunked():
+        for chunk in inputs[0].chunked(1000):               # Rucio can not handle more than 1000 dids at a time
             chunk_files = {f.did(): f for f in chunk}
             dids = [{"scope":f.Namespace, "name":f.Name} for f in chunk]
             for f in chunk_files.values():
                 f.Metadata["rucio.rses"] = []
 
-            replicas = client.list_replicas(dids, all_states=False, ignore_availability=False,
-                resolve_archives=False)
+            replicas = client.list_replicas(dids, all_states=False, ignore_availability=False, resolve_archives=False)
 
             for r in replicas:
                 did = "%(scope)s:%(name)s" % r
