212ca9630b2a56c29a3d7db6a26d8d792111d079
Igor Mandrichenko <igorvm@gmail.com>
2023-07-17 05:30:12 -0500

    Fixed in conversion scripts

--- conversion/index_files.sh
+++ conversion/index_files.sh
@@ -10,10 +10,6 @@ drop index if exists files_names, files_name, files_size,
 \echo ... building files namespace:name index ...
 create unique index files_names on files(namespace, name) include (id);
 create index files_name on files(name) include (namespace, id);
-create index files_size on files(size);
-
-
-create index files_creator on files(creator);
 create index files_created_timestamp on files(created_timestamp);
 
 _EOF_
